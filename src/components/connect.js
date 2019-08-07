cab6e702676787e96cdd7e2c4b5bb810e4549ee5
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2019-08-07 14:31:26 -0400

    added SSL key and csr, updated LinkIn link

--- src/components/connect.js
+++ src/components/connect.js
@@ -12,7 +12,7 @@ export class Connect extends React.Component {
                     <h2>Connect</h2>
                     <p>I am looking for a tight-knit community of coders. I have big ideas, but I am ready to start small.</p>
                     <div className="connectLinks">
-                        <a className="linkedInLink" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/aaron-turkel-395965172">Linked In</a>
+                        <a className="linkedInLink" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/aaron-turkel">Linked In</a>
                         <a className="gmailLink" target="_blank" rel="noopener noreferrer" href="mailto:theholycoder@gmail.com">Email</a>
                         <a className="twitterLink" target="_blank" rel="noopener noreferrer" href="https://twitter.com/TheHolyCoder">Twitter</a>
                         <a className="gitHubLink" target="_blank" rel="noopener noreferrer" href="https://github.com/AaronDovTurkel">GitHub</a>
